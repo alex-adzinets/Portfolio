6c6baca5f281132a7f811cd0ad06b73c5781480d
Alex Adzinets <alexadzinets@Aliaksandrs-MacBook-Pro.local>
2025-05-28 20:14:02 +0200

    Two different bootstrap files

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,17 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <div style={{ 
+      color: "#ffffff", 
+      fontSize: 65, 
+      fontFamily: "fantasy", 
+      fontWeight: "bold", 
+      width: "100%", 
+      textAlign: "center", 
+      alignSelf: "center", 
+      alignContent:"center", 
+      alignItems:"center" }}>TROLOLOLO!!!</div>
+      
     <App />
   </React.StrictMode>,
 )
