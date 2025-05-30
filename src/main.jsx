ab1fb470d95a7053e0e7adc2ee2f64c080ab77c8
Alex Adzinets <alexadzinets@Aliaksandrs-MacBook-Pro.local>
2025-05-30 11:27:28 +0200

    fixcomment

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       textAlign: "center", 
       alignSelf: "center", 
       alignContent:"center", 
-      alignItems:"center" }}>TROLOLOLO!!!</div>
+      alignItems:"center" }}>This line is from main.jsx. There is no such line in main2.jsx!!!</div>
       
     <App />
   </React.StrictMode>,
