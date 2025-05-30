8c0d175401c74e79397bd119ba8ffe84a8c9e221
Alex Adzinets <alexadzinets@Aliaksandrs-MacBook-Pro.local>
2025-05-30 14:20:22 +0200

    UCE-3-6

--- vitest.setup.js
+++ vitest.setup.js
@@ -1,7 +1,11 @@
-import { expect } from 'vitest';
-
+import { expect, afterEach } from 'vitest';
+import { cleanup } from '@testing-library/react';
 import * as matchers from "@testing-library/jest-dom/matchers";
 
 console.log('Available matchers:', matchers);
 
 expect.extend(matchers);
+
+afterEach(() => {
+  cleanup();
+});
